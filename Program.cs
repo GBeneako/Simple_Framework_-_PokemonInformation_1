c3b881cf2a956a9880b5e45659a72b0680e4d79d
GGentleman <78107279+GGentleman@users.noreply.github.com>
2022-03-04 23:58:36 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gilbert_s_Pokemon_Stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var trainer = new Trainer(123357, "Gilbert Emmall", "Littleroot Town, Hoenn", "Treecko", 10, "YES - Done and electronically signed by Professor Birch", "None");
+
+            var PokémonTrainerID = 1233578;
+
+            // 12 - Lord's disciples and complete* or 1 - God is one, 3 - Triune God and 3 1, 5 - responsibility, and 7 - complete, perfect* or seven spirits, 8 - resurrection,
+            Console.WriteLine("\n \nPokémon Trainer ID: " + PokémonTrainerID);
+            Console.WriteLine("Pokémon Trainer's name: " + "Gilbert Emmall");
+            Console.WriteLine("The Pokémon Trainer's Age: " + "10");
+            Console.WriteLine("Hometown: " + "Littleroot Town, Hoenn");
+            Console.WriteLine("Partner Pokémon: " + "Treecko");
+            //Console.WriteLine("Graduated/Finished School: " + "\n      ");
+            Console.WriteLine("Registered for the Hoenn League?: " + "\n        YES - Done and electronically signed by Professor Birch");
+            Console.WriteLine("How many badges currently obtained for the Hoenn League: " + "None");
+        }
+    }
+}
